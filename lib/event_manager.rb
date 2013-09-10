1d76b39be3a5e5a63ca179646d52375b05ee30af
Ben Lewis <bennlewis@gmail.com>
2013-09-10 15:44:15 -0600

    Created event manager, added sunlight gem

---
+++ lib/event_manager.rb
@@ -0,0 +1,22 @@
+require 'csv'
+require 'sunlight/congress'
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+puts "EventManager Initialized!"
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+
+  name = row[:first_name]
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
+end
