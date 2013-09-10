fc74db5a86eb99275d9ad9afa459f737c8402d4e
Rolen Le <rolentle@gmail.com>
2013-09-10 16:45:04 -0600

    changed the formart of personal letters to erb

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -9,30 +10,24 @@ end
 
 def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-
-  legislator_names.join(", ")
 end
 
 
 puts "EventManager Initialized!"
 
-template_letter = File.read "form_letter.html"
-
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
-contents.each do |row|
 
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
+contents.each do |row|
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
-  legislators = legislators_by_zipcode(zipcode).join(', ')
+  legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME', name)
-  personal_letter.gsub!('LEGISLATORS', legislators)
+  form_letter = erb_template.result(binding)
 
-  puts personal_letter
+  puts form_letter
 end
