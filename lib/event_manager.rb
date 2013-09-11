e741f2991134b340badce2cf5954394e46b97777
Rolen Le <rolentle@gmail.com>
2013-09-11 13:44:14 -0600

    Added clean phone number method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,6 +8,17 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def clean_phone_number(phone_number)
+  clean_number = phone_number.gsub(/\D/, "")
+  if clean_number.length == 11 && clean_number[0] == "1"
+    clean_number = clean_number[1..-1]
+  elsif clean_number.length != 10        
+    clean_number = "0000000000"
+  else 
+    clean_number
+  end
+end    
+
 def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
