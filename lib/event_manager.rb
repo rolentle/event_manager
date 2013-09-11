f6fa499693ebc62f1fad96d9f7717a7d804e0fb0
Ben Lewis <bennlewis@gmail.com>
2013-09-11 14:36:24 -0600

    Created popular_hour method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -12,12 +12,28 @@ def clean_phone_number(phone_number)
   clean_number = phone_number.gsub(/\D/, "")
   if clean_number.length == 11 && clean_number[0] == "1"
     clean_number = clean_number[1..-1]
-  elsif clean_number.length != 10        
+  elsif clean_number.length != 10
     clean_number = "0000000000"
-  else 
+  else
     clean_number
   end
-end    
+end
+
+def clean_date(date)
+  old_date = DateTime.strptime(date, "\%m\/\%e\/\%y\s\%H:\%M")
+end
+
+def popular_hour(dates)
+  datehash = {}
+  dates.each do |date|
+    datehash[clean_date(date).hour] ||= 0
+    datehash[clean_date(date).hour] += 1
+  end
+  dirtyarray = datehash.values.to_a
+  dirtyarray.sort!
+  return datehash.key(dirtyarray[-1])
+end
+
 
 def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
